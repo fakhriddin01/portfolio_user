127bb514ae2044feb07de32af5fcc3d6e16ddaf4
fakhriddin01 <a.fakhriddin@gmail.com>
2023-06-21 17:57:59 +0500

    axios

--- src/service/axios.js
+++ src/service/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://myportfoliobackend-production.up.railway.app/api"
+axios.defaults.baseURL = "https://myportfoliobackend-production.up.railway.app/api/"
 
 export default axios
