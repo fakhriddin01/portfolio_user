8ac7b1470c0908a2ab7b3dc5bec7babcc8686390
fakhriddin01 <a.fakhriddin@gmail.com>
2023-07-03 11:48:07 +0500

    vercel analyticts cancel intect in main.js

--- src/main.js
+++ src/main.js
@@ -5,10 +5,8 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-import { inject } from '@vercel/analytics';
 
 const app = createApp(App)
-inject();
 
 app.use(createPinia())
 app.use(router)
