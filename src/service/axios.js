7cadcb354224f67fbaad7a49b0c12d3bc4ed6a1e
fakhriddin01 <a.fakhriddin@gmail.com>
2023-08-27 20:04:56 +0500

    update server to AWS

--- src/service/axios.js
+++ src/service/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://myportfoliobackend-production.up.railway.app/api/"
+axios.defaults.baseURL = "http://16.171.43.128:4000/api/"
 
 export default axios
